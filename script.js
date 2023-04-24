7be9ae0ae2b2ba19fb9c376039492816d5fa748f
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-24 16:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,79 @@
 //your code here
-const imageContainer = querySelector(".image-container");
-const imageClass = ['img1','img2','img3','img4','img5'];
+const imageContainer = document.querySelector(".image-container");
+const images = document.getElementsByTagName("img");
+const resetBtn = document.getElementById("reset");
+const verifyBtn = document.getElementById("verify");
+const para = document.getElementById("para");
+
 document.addEventListener("DOMContentLoaded",()=>{
-	const image = document.createElement("img");
-	image.style.backgroundColor = "yellow";
-	imageContainer.appendChild(image);
-})
+	let repeatInd = Math.floor(Math.random()*5);
+	let classNameRepeat = images[repeatInd].className;
+	for(let i=images.length-2;i>=0;i--){
+		let ind = Math.floor(Math.random()*(i+1));
+		let class1 = images[i].className;
+		let class2 = images[ind].className;
+		images[i].classList.remove(class1);
+		images[i].classList.add(class2);
+		images[ind].classList.remove(class2);
+		images[ind].classList.add(class1);
+	}
+
+	images[5].classList.add(classNameRepeat);
+});
+
+let clickCount = 0;
+const imgSelected = [];
+
+imageContainer.addEventListener("click",(e)=>{
+	if(e.target.tagName === "IMG"){
+		if(!imgSelected.includes(e.target.className)){
+			e.target.classList.add("selected");
+			imgSelected.push(e.target.className);
+			clickCount++;
+		}
+		else{
+			let ind = imgSelected.indexOf(e.target.className);
+			imgSelected.splice(ind,1);
+			e.target.classList.remove("selected");
+			clickCount--;
+		}
+
+		console.log(imgSelected);
+
+		if(clickCount>=1){
+			resetBtn.style.display = "block";
+		}
+
+		if(clickCount===2){
+			verifyBtn.style.display = "block";
+		}
+		else if(clickCount<2 || clickCount>2){
+			verifyBtn.style.display = "none";
+		}
+	}
+});
+
+resetBtn.addEventListener("click",()=>{
+	for(let i=0;i<images.length;i++){
+		images[i].classList.remove("selected");
+	};
+	para.style.display = "none";
+	resetBtn.style.display = "none";
+	verifyBtn.style.display = "none";
+});
+
+verifyBtn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	if(imgSelected.length===2){
+		para.style.display = "block";
+		let firstClass = imgSelected[0].split(" ");
+		let secClass = imgSelected[1].split(" ");
+		if(firstClass[0]===secClass[0]){
+			verifyBtn.style.display = "none";
+			para.innerText = "You are a human. Congratulations!";
+		}
+		else{
+			para.innerText = "We can't verify you as a human. You selected the non-identical tiles. ";
+		}
+	}
+});
