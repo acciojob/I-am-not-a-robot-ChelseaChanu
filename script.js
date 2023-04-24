2f37367c6043e89684ce6342688966ff6c39a6fe
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-24 14:14:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const imageContainer = querySelector(".image-container");
+const imageClass = ['img1','img2','img3','img4','img5'];
+document.addEventListener("DOMContentLoaded",()=>{
+	const image = document.createElement("img");
+	image.style.backgroundColor = "yellow";
+	imageContainer.appendChild(image);
+})
